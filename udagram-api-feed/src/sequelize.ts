7b248e777d01699d8c496e8f3fb567aa53f59c10
Kamogelo Lamola <lamola.kamogelo@gmail.com>
2022-09-23 20:37:10 +0200

    yet another commit

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,12 +1,11 @@
 import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+  'username': "postgres",
+  'password': "password",
+  'database': "postgres",
+  'host': "udagram-db.cpuoz88bjhgj.us-east-2.rds.amazonaws.com",
 
-  'dialect': config.dialect,
+  'dialect': "postgres",
   'storage': ':memory:',
 });
