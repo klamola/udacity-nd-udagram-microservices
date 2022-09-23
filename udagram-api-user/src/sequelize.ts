4c111c3c7b8fe0690703662ad66c4125ec1b83bc
Kamogelo Lamola <lamola.kamogelo@gmail.com>
2022-09-23 20:02:17 +0200

    yet another config file update

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -4,7 +4,7 @@ export const sequelize = new Sequelize({
   'username': "postgres",
   'password': "password",
   'database': "postgres",
-  'host': "udagram-db.cii0cgoo5lpk.us-east-1.rds.amazonaws.com",
+  'host': "udagram-db.cpuoz88bjhgj.us-east-2.rds.amazonaws.com",
 
   'dialect': "postgres",
   'storage': ':memory:',
