42c93efdc7420f734ef407466a0d2927ecd3a504
Kamogelo Lamola <lamola.kamogelo@gmail.com>
2022-10-01 11:01:36 +0200

    config changes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ac0c9cac2108f420290f5c6195ff8845-287422927.us-west-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
